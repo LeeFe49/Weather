1c5266aa63832fa7f61298a37d0d6d40d24f44fe
LeeFe49 <chullee49@naver.com>
2026-07-21 00:15:17 +0900

    fix: DB 연결 환경설정 수정

--- src/main/java/com/leefe/weather/repository/JdbcMemoRepository.java
+++ src/main/java/com/leefe/weather/repository/JdbcMemoRepository.java
@@ -16,11 +16,11 @@ public class JdbcMemoRepository {
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    private JdbcMemoRepository(DataSource dataSource) {
+    public JdbcMemoRepository(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public Memo Save(Memo memo) {
+    public Memo save(Memo memo) {
         String sql = "insert into memo values(?,?)";
         jdbcTemplate.update(sql, memo.getId(), memo.getText());
         return memo;
